305b8356d90ea29a3de4171dee8e67f37e5ad691
g-barbosa <giovanne.bbarbosa@gmail.com>
2020-02-22 17:05:48 -0300

    deploy no heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const Telegraf = require('telegraf')
 const fs = require('fs')
 const {printClass} = require('./printClass')
+const express = require('express')
+const expressApp = express()
 require('dotenv/config')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const API_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://bot-aulas.herokuapp.com/';
 
 bot.start((ctx) => ctx.reply('Hello world'))
 bot.command('aulas', async(ctx) => {
@@ -18,4 +23,5 @@ bot.command('aulas', async(ctx) => {
     ctx.replyWithPhoto({ source: fs.createReadStream('./aulas.png') });
 })
 
-bot.telegram.setWebhook('https://bot-aulas.herokuapp.com')
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
