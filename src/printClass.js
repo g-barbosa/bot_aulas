ae1e5d3da128131b671663f29a323fcdb0545bda
g-barbosa <giovanne.bbarbosa@gmail.com>
2020-02-22 20:06:19 -0300

    added a image host

--- src/printClass.js
+++ src/printClass.js
@@ -1,9 +1,21 @@
 const puppeteer = require('puppeteer')
+const cloudinary = require('cloudinary')
 require('dotenv/config')
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
 
 const classPage = 'https://account.impacta.edu.br/aluno/quadro-horario.php?turmaid=MDE2TVRVNE1qTTVNekE1TWc9PU5qUXdNdz09&produto=MDE2TVRVNE1qTTVNekE1TWc9PU5UYz0='
 
 const printClass = async() => {
+    
+    const cloudinary_options = { 
+      public_id: `aulas`
+    };
+
     const browser = await puppeteer.launch({
         args: [
           '--no-sandbox',
@@ -19,13 +31,39 @@ const printClass = async() => {
         page.waitForNavigation({ waitUntil: 'networkidle0' }),
 ]);
     await page.keyboard.press('Escape')
-const classPage = 'https://account.impacta.edu.br/aluno/quadro-horario.php?turmaid=MDE2TVRVNE1qTTVNekE1TWc9PU5qUXdNdz09&produto=MDE2TVRVNE1qTTVNekE1TWc9PU5UYz0='
     await page.goto(classPage, { waitUntil: 'networkidle0' })
-    await page.screenshot({
-        path: 'aulas.png',
+    
+    let shotResult = await page.screenshot({
         fullPage: true
-    })
+    }).then((result) => {
+        console.log(`got some results.`);
+        return result;
+    }).catch(e => {
+        console.error(`Error in snapshotting news`, e);
+        return false;
+    });
+
     await browser.close()
+
+    if (shotResult){
+        return cloudinaryPromise(shotResult, cloudinary_options);
+      }else{
+        return null;
+      }
+
+    function cloudinaryPromise(shotResult, cloudinary_options){
+        return new Promise(function(res, rej){
+          cloudinary.v2.uploader.upload_stream(cloudinary_options,
+            function (error, cloudinary_result) {
+              if (error){
+                console.error('Upload to cloudinary failed: ', error);
+                rej(error);
+              }
+              res(cloudinary_result);
+            }
+          ).end(shotResult);
+        });
+      }
 }
 
 module.exports = printClass()
