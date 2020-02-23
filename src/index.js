2378e8e47f00aa7c33f8330343d3cc022c527a33
g-barbosa <giovanne.bbarbosa@gmail.com>
2020-02-23 02:07:51 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ bot.start((ctx) => ctx.reply('Hello world'))
 bot.command('sobre', (ctx) => ctx.reply('bot para pegar informações das aulas'))
 bot.command('aulas', async(ctx) => {
     ctx.reply('Vou verificar para você')
-    const aulas = await printClass
+    const aulas =  printClass
     ctx.reply(aulas[0].materia)
 })
 
