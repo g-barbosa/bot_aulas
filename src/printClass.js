8c824d5080c82078f4852c2613daebd53c89610b
g-barbosa <giovanne.bbarbosa@gmail.com>
2020-02-23 01:31:47 -0300

    teste

--- src/printClass.js
+++ src/printClass.js
@@ -2,26 +2,19 @@ const puppeteer = require('puppeteer')
 const cloudinary = require('cloudinary')
 require('dotenv/config')
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
-})
-
 const classPage = 'https://account.impacta.edu.br/aluno/quadro-horario.php?turmaid=MDE2TVRVNE1qTTVNekE1TWc9PU5qUXdNdz09&produto=MDE2TVRVNE1qTTVNekE1TWc9PU5UYz0='
 
 const printClass = async() => {
     
-    const cloudinary_options = { 
-      public_id: `aulas`
-    };
-
     const browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ],
-      });
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--single-process'
+      ],
+    });
     const page = await browser.newPage()
     await page.goto('https://account.impacta.edu.br/', { waitUntil: 'networkidle0' })
     await page.type('#deslogin', process.env.RA);
