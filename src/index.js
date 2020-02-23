cfe15cc5d57c3e60b3bc91f01b975be6ce079b1f
g-barbosa <giovanne.bbarbosa@gmail.com>
2020-02-23 12:06:42 -0300

    recebendo dados de todas as aulas

--- src/index.js
+++ src/index.js
@@ -4,13 +4,40 @@ require('dotenv/config')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply('Hello world'))
-bot.command('sobre', (ctx) => ctx.reply('bot para pegar informações das aulas'))
+bot.start((ctx) => ctx.reply('O QUE DESEJA SABER?'))
 bot.command('aulas', async(ctx) => {
     ctx.reply('Vou verificar para você')
     const aulas =  await printClass
-    console.log(aulas)
-    ctx.reply(aulas[0].materia)
+    ctx.reply(`
+    SEGUNDA:\n
+    MATERIA: ${aulas[0].materia}\n
+    PROF: ${aulas[0].prof}\n
+    SALA1: ${aulas[0].sala1}\n
+    SALA2: ${aulas[0].sala2}`)
+    ctx.reply(`
+    TERÇA:\n
+    MATERIA: ${aulas[1].materia}\n
+    PROF: ${aulas[1].prof}\n
+    SALA1: ${aulas[1].sala1}\n
+    SALA2: ${aulas[1].sala2}`)
+    ctx.reply(`
+    QUARTA:\n
+    MATERIA: ${aulas[2].materia}\n
+    PROF: ${aulas[2].prof}\n
+    SALA1: ${aulas[2].sala1}\n
+    SALA2: ${aulas[2].sala2}`)
+    ctx.reply(`
+    QUINTA:\n
+    MATERIA: ${aulas[3].materia}\n
+    PROF: ${aulas[3].prof}\n
+    SALA1: ${aulas[3].sala1}\n
+    SALA2: ${aulas[3].sala2}`)
+    ctx.reply(`
+    SEXTA:\n
+    MATERIA: ${aulas[4].materia}\n
+    PROF: ${aulas[4].prof}\n
+    SALA1: ${aulas[4].sala1}\n
+    SALA2: ${aulas[4].sala2}`)
 })
 
 bot.launch({
